d4899e750a361483fc7402f51e9b1e94bf41ee29
mwaqarsikandar <56826024+mwaqarsikandar@users.noreply.github.com>
2020-02-10 17:21:59 +0500

    Donw

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./styles.css";
 
 function Hi() {
-  return <div>Hello World!</div>;
+  return <div>Hello World! WOW</div>;
 }
 
-ReactDOM.render(<Hi/>, document.querySelector('#root'));
-
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandkbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+ReactDOM.render(<Hi />, document.querySelector("#root"));
